edd7943df35308f00c58c867ba5b70351c86b295
Justin Kanwischer <kanwischer.justinc@gmail.com>
2021-05-07 19:58:05 +0200

    main dashboard styling

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,24 +1,38 @@
 import '../scss/dashboard/dashboard.scss';
-import logo from '../asset/images/piggy-bank-euro.svg';
 import Logo from '../components/logo.js';
+import React, { useState } from "react";
 
 function Dashboard() {
+	const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <div className="wrapper">
-      <Logo />
-      <div class="sidebar">
-      	<div class="container">
-      		<div class="menu-burger">
-      			<span></span>
-      			<span></span>
-      			<span></span>
-      		</div>
-      		<div class="settings">
-      			
-      		</div>
-      	</div>
-      </div>
-    </div>
+  	<div class="dashboard">
+		  <div className={`sidebar ${isActive ? "active" : "closed"}`}>
+		  	<div className="container">
+		  		<div className="menu-burger" onClick={handleToggle}>
+		  			<span></span>
+		  			<span></span>
+		  			<span></span>
+		  		</div>
+		  		<div className="settings">
+		  			<div className="settings-img"></div>
+		  		</div>
+		  	</div>
+		  </div>
+		  <div class="main-section">
+		  	<Logo />
+		  	<div class="main-dashboard">
+		  		<div class="budget-component-here"><h1>Budget</h1></div>
+		  		<div class="expense-component">
+	  				<h2>test</h2>
+		  		</div>
+		  	</div>
+		  </div>
+		</div>
   );
 }
 
